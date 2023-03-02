96aaa0e96f37cbd3357d3861eb1106f1dc90ffb4
fbaun <fbaun@HIGHTOWER>
2023-03-02 22:17:05 +0100

    -

---
+++ Gymnastikforening/SvoemmeHold.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymnastikForening
+{
+    public class SvoemmeHold : Hold
+    {
+        public string SvoemmeHal { get; set; }
+
+        public SvoemmeHold(string holdId, int år, string holdNavn, double prisPrDeltager, int maxAntalBørn, string svoemmehal):base(holdId,år,holdNavn,prisPrDeltager,maxAntalBørn)
+        {
+            SvoemmeHal = svoemmehal;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\n\tSvømmehal {SvoemmeHal}";
+        }
+
+    }
+}
